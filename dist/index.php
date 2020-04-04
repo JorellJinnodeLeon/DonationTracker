5651086ace56bbf38ce1e7b8200cf3846f90c125
Jorell de Leon <jorelljinnodeleon@gmail.com>
2020-04-05 04:23:52 +0800

    Update index.php

--- dist/index.php
+++ dist/index.php
@@ -185,7 +185,13 @@
                                             </tr>
                                         </tfoot>
                                         <tbody>
-
+                                            <tr>
+                                                <td>Tiger Nixon</td>
+                                                <td>System Architect</td>
+                                                <td>Edinburgh</td>
+                                                <td>61</td>
+                                                <td>2011/04/25</td>
+                                            </tr>
                                         </tbody>
                                     </table>
                                 </div>
