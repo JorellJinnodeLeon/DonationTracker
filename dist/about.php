56c3ec6d9169664b2c4f660622f31bd3a145bd4f
Jorell de Leon <jorelljinnodeleon@gmail.com>
2020-04-05 06:44:04 +0800

    Update about.php

--- dist/about.php
+++ dist/about.php
@@ -13,6 +13,7 @@
                         <div class="card mb-4">
                             <div class="card-header"><i class="fas fa-table mr-1"></i>The Organization</div>
                             <div class="card-body">
+                                <!--
                                 <div class="text-center">
                                     <img src="./assets/img/bayanihan.jpg" class="img-fluid" alt="Responsive image">
                                 
@@ -37,6 +38,31 @@
                                         <br />
                                         Looking forward to creating a difference with you! 
                                     </p>
+                                </div>
+                                    -->
+                                <div class="card" style="width: 70%;">
+                                    <img class="card-img-top" src="./assets/img/bayanihan.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <p class="card-text">Magandang Araw! <br />
+
+                                            We can do something beyond locking ourselves in our homes this quarantine season.<br />
+
+                                            This group is created to provide a venue for those who want to help in their own ways our front liners. How does this group work?
+                                            <br />
+                                            We connect all the key points necessary to help our front liners which include material suppliers, labor, delivery/logistics and donors. We already have direct connection with National Book Store, producers/suppliers of face masks, face shields and aerosol containers.
+                                            <br />  
+                                            We also have direct contacts to some hospitals who need help. Apparently, government hospitals are not encouraged to actively seek donations and so we are coordinating directly with chief resident doctors for the help.
+                                            <br />
+                                            We have 3 bank accounts where you can deposit your help. We are creating a google form that you can fill up to allow us to track the incoming donation. This is led by Coach Meg Ramoso
+                                            We also have a google sheet that you can view to check the progress of the donation and the names of those who gave donations with the corresponding value. You may opt to use a pseudonym in the google form if you wish anonymity. 
+                                            No donation is too small nor too large. 10, 20, 100 or 1000 pesos will go a long way to help our healthcare front liners.
+                                            <br />
+                                            We will also show the breakdown and the relevant receipts for the expenses related to this activity.
+                                            <br />
+                                            Should you have questions or clarifications, please feel free to reach out to me. From time to time, we will also hold zoom meetings and update you guys on the progress. 
+                                            <br />
+                                            Looking forward to creating a difference with you! </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
