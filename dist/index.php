8d002d3c176c719058943ad2483792d82b6f3691
Jorell de Leon <jorelljinnodeleon@gmail.com>
2020-04-04 22:57:59 +0800

    Update index.php

--- dist/index.php
+++ dist/index.php
@@ -59,7 +59,7 @@
 
             
             var table = document.getElementById("dataTable");
-            var row = table.insertRow(0);
+            var row = table.insertRow(1);
             var cell0 = row.insertCell(0);
             var cell1 = row.insertCell(1);
             var cell2 = row.insertCell(2);
